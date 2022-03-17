672afbbba8139e9f2b9add41cdf39cefabbb62d0
markmakuno <makunomark@gmail.com>
2022-03-17 21:42:47 +0300

    Add solution to part b.

--- 2015/day-one/index.ts
+++ 2015/day-one/index.ts
@@ -1,11 +1,61 @@
 import { readFileToString } from "../../utils/readFile";
 import path from "path";
 
+const { env } = process.env;
+
 async function main() {
-  const stringInput = await readFileToString(
-    path.join(__dirname, "sample_input.txt")
+  const input = await readFileToString(
+    path.join(
+      path.join(__dirname, "inputs"),
+      env === "test" ? "sample_input.txt" : "input.txt"
+    )
   );
-  console.log(stringInput);
+
+  if (input) {
+    const parentheses = input.split("");
+    const floor = findFloor(parentheses);
+    console.log("Part A:", floor);
+    const characterPosition = findFirstBasementPosition(parentheses);
+    console.log("Part B:", characterPosition);
+  }
+}
+
+function findFloor(parentheses: string[]): number {
+  let floor = 0;
+
+  parentheses.forEach((value) => {
+    if (value === "(") {
+      ++floor;
+    } else if (value === ")") {
+      --floor;
+    } else {
+      // we do not understand this character. Skip :)
+    }
+  });
+  return floor;
+}
+
+function findFirstBasementPosition(parentheses: string[]): number {
+  let floor = 0;
+  let characterPosition: number = 0;
+
+  parentheses.every((value, position) => {
+    if (value === "(") {
+      ++floor;
+    } else if (value === ")") {
+      --floor;
+    } else {
+      // we do not understand this character. Skip :)
+    }
+
+    if (floor === -1) {
+      characterPosition = position;
+      return false;
+    }
+    return true;
+  });
+
+  return characterPosition + 1;
 }
 
 main();
--- README.md
+++ README.md
@@ -1,7 +1,11 @@
 ## Folder structure
+
 Solutions are grouped by year and later inside folders labelled buy number `day-<number>`. For instance the solution for day one 2015 will be in the folder `/2015/day-one` and `/2015/day-two` for the second day and so on.
 
+In each day, inside the `inputs` folder, there are two files, `sample_input.txt` and `input.txt` which hold the input that we get from the questions. The file with `sample` prepended holds the sample input, and the other one holds the actual input. To choose when to use sample input or actual input, we use environment variable named `env`. In before running step 3 in [Running locally](#running-locally), run `export env=prod` to run aganist the atcual input and `export env=test` aganist the sample input.
+
 ## Running locally
+
 1. Install dependencies by running `$npm i` in the root folder.
-1. Navigate to the problem you want to run. 
+1. Navigate to the problem you want to run.
 1. Run the solution by running `$npx ts-node index.ts`
