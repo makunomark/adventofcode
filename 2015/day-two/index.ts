4674b577d972b8d560b4023d813b515905e5ccdf
markmakuno <makunomark@gmail.com>
2022-03-21 23:57:20 +0300

    Solved for 2015 day 2

--- 2015/day-two/index.ts
+++ 2015/day-two/index.ts
@@ -11,28 +11,34 @@ async function main() {
     )
   );
 
+  let inputArray: String[] | null = null;
   if (input) {
-    const partASoln = partA(input);
-    console.log({ partASoln });
+    inputArray = input.split("\n' +").join().split("\n");
   }
 
-  const input_b = await readFileToString(
-    path.join(
-      path.join(__dirname, "inputs/part_b"),
-      env === "test" ? "sample_input.txt" : "input.txt"
-    )
-  );
+  if (inputArray) {
+    const partASoln = partA(inputArray);
+    console.log("Part A:", partASoln);
+  }
 
-  if (input_b) {
-    // const partBSoln = partB(input);
+  // Reading from solution A's input
+  // const input_b = await readFileToString(
+  //   path.join(
+  //     path.join(__dirname, "inputs/part_b"),
+  //     env === "test" ? "sample_input.txt" : "input.txt"
+  //   )
+  // );
+
+  if (inputArray) {
+    const partBSoln = partB(inputArray);
+    console.log("Part A:", partBSoln);
   }
 }
 
-function partA(input: any): any {
-  const inputArray = input.split("\n' +").join().split("\n");
+function partA(input: String[]): Number {
   let totalSquareFeet = 0;
 
-  inputArray.forEach((element: string) => {
+  input.forEach((element: String) => {
     const dimensions = element.split("x");
     const l = Number(dimensions[0]);
     const w = Number(dimensions[1]);
@@ -51,6 +57,38 @@ function partA(input: any): any {
   return totalSquareFeet;
 }
 
-function partB(args: any): any {}
+function partB(input: String[]): Number {
+  let totalRibbonFeet = 0;
+
+  input.forEach((element: String) => {
+    const dimensions = element.split("x");
+    const l = Number(dimensions[0]);
+    const w = Number(dimensions[1]);
+    const h = Number(dimensions[2]);
+
+    const cubicVolume = volume(l, w, h);
+    const perimeter = perimeterOfShortestSide(l, w, h);
+    const ribbonFeet = cubicVolume + perimeter;
+
+    totalRibbonFeet += ribbonFeet;
+  });
+
+  return totalRibbonFeet;
+}
 
 main();
+
+function volume(l: number, w: number, h: number): number {
+  return l * w * h;
+}
+function perimeterOfShortestSide(l: number, w: number, h: number): number {
+  const array = [l, w, h];
+  const max = Math.max(l, w, h);
+
+  const index = array.findIndex((value: number) => value == max);
+  array.splice(index, 1);
+
+  const perimeter = 2 * array[0] + 2 * array[1];
+
+  return perimeter;
+}
