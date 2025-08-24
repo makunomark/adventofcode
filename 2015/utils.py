875468d3c7bcf38727ac23150665ce8d0dfc0380
Mark Makuno <makunomark@gmail.com>
2025-08-24 17:36:21 +0200

    Refactor is_example function to handle invalid input and loop until a valid response is given

--- 2015/utils.py
+++ 2015/utils.py
@@ -1,5 +1,9 @@
 def is_example():
-  resp = input("Running example? (Y/n)").lower().strip()
-  if not resp or resp == "y":
-    return True
-  return False
+  while True:
+    resp = input("Running example? (Y/n)").lower().strip()
+    if not resp or resp == "y":
+      return True
+    elif resp == "n":
+      return False
+    else:
+      print("Invalid selection. Input n or y.")
