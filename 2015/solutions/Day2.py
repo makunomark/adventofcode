ce80a2b0e9811aa1c4e8945983af343bed231861
Mark Makuno <makunomark@gmail.com>
2025-08-26 12:57:15 +0200

    Add solution for Day 2 part b and input file

--- 2015/solutions/Day2.py
+++ 2015/solutions/Day2.py
@@ -6,6 +6,9 @@ class Day2(Solution):
   example_input_a = "1x1x10" # 1x1x10
   input_a_file = INPUTS_DIR / "day_2_a.txt"
 
+  example_input_b = "1x1x10"
+  input_b_file = INPUTS_DIR / "day_2_b.txt"
+
   def calculate_surface_area(self, dims):
     if len(dims) != 3:
       return 0
@@ -21,3 +24,16 @@ class Day2(Solution):
       area = self.calculate_surface_area(dims)
       total += area
     print("a) Total surface area (with slack): ", total)
+
+  def sol_b(self):
+    super().sol_b()
+    total = 0
+    for line in self.input_b_data.split('\n'):
+      dims = [int(x) for x in line.split('x')]
+      l, w, h = dims
+      bow = l * w * h
+      dims.remove(max(dims))
+      a, b = dims
+      present_wrapper = (2 * a) + (2 * b)
+      total += present_wrapper + bow 
+    print("b) Total feet of ribbon:", total)
